9759d675ebcdfac205ee6c34358c4c2661227618
Joaquinn <joaov.b.euwest@gmail.com>
2025-03-26 11:52:42 -0300

    #init

--- saudacao.test.js
+++
@@ -1,9 +0,0 @@
-const saudacao = require('./saudacao')
-
-describe('Testes da função dizOla', () => {
-  test('deve retornar "Olá João" quando o nome for João', () => {
-    const olaJoao = saudacao.dizOla('João')
-    expect(olaJoao).toBe('Olá João')
-    expect(true).toBeTruthy();
-  })
-})
